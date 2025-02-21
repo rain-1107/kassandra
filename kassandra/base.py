8eed28ba3b3c67b29ebc4afdd9ac07033413d3a1
rain-1107 <83094739+rain-1107@users.noreply.github.com>
2025-02-21 14:51:20 +0000

    Revert "Prototype"

--- kassandra/base.py
+++
@@ -1,9 +0,0 @@
-from _collections_abc import Callable
-from typing import Self 
-
-class Base:
-    def __init__(self):
-        self._update_callback: Callable[[Self], None] | None = None 
-
-    def set_update_callback(self, update_callback: Callable[[Self], None]) -> None:
-        self._update_callback = update_callback
